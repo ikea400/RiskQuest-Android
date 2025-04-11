ee027ffd57f1d9e7975bdff9a0159e89d21a7b73
lilianfelix-prog <126830086+lilianfelix-prog@users.noreply.github.com>
2025-04-11 17:27:54 -0400

    - missing file

--- app/utililty/utils.ts
+++ app/utililty/utils.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 export function arrayEquals(a1: any[], a2: any[]) {
   if (a1.length !== a2.length) {
     return false;
@@ -8,4 +10,14 @@ export function arrayEquals(a1: any[], a2: any[]) {
     if (a1[i] !== a2[i]) return false;
   }
   return true;
+}
+
+export function formatDate(dateString: string) {
+  if (!dateString) return 'Date N/A';
+  try {
+    return format(new Date(dateString), 'YYYY-MM-DD HH:mm:ss');
+  } catch (error) {
+    console.error("Error formatting date:", error);
+    return dateString; 
+  }
 }
